3ec471e73b4bd3cd8a9f35b5ca3f406726365ba8
pukekakaster <pukekakaster@gmail.com>
2018-01-23 14:13:26 +0900

    initial commit

---
+++ mann/util.py
@@ -0,0 +1,153 @@
+import numpy as np
+import os
+from PIL import Image
+from PIL import ImageOps
+import random
+
+def generate_random_strings(batch_size, seq_length, vector_dim):
+    return np.random.randint(0, 2, size=[batch_size, seq_length, vector_dim]).astype(np.float32)
+
+
+def one_hot_encode(x, dim):
+    res = np.zeros(np.shape(x) + (dim, ), dtype=np.float32)
+    it = np.nditer(x, flags=['multi_index'])
+    while not it.finished:
+        res[it.multi_index][it[0]] = 1
+        it.iternext()
+    return res
+
+
+def one_hot_decode(x):
+    return np.argmax(x, axis=-1)
+
+
+def five_hot_decode(x):
+    x = np.reshape(x, newshape=np.shape(x)[:-1] + (5, 5))
+    def f(a):
+        return sum([a[i] * 5 ** i for i in range(5)])
+    return np.apply_along_axis(f, -1, np.argmax(x, axis=-1))
+
+
+def baseN(num,b):
+    return ((num == 0) and  "0" ) or ( baseN(num // b, b).lstrip("0") + "0123456789abcdefghijklmnopqrstuvwxyz"[num % b])
+
+
+class SampleDataLoader:
+    def __init__(self, data_dir, image_size=(20, 20), n_train_classses=1200, n_test_classes=423):
+        self.data = []
+        self.image_size = image_size
+
+        for dirname, subdirname, filelist in os.walk(data_dir):
+            if filelist:
+                self.data.append(
+                    # [np.reshape(
+                    #     np.array(Image.open(dirname + '/' + filename).resize(image_size), dtype=np.float32),
+                    #     newshape=(image_size[0] * image_size[1])
+                    #     )
+                    #     for filename in filelist]
+                    # [io.imread(dirname + '/' + filename).astype(np.float32) / 255 for filename in filelist]
+                    [Image.open(dirname + '/' + filename).copy() for filename in filelist]
+                )
+
+        # print(np.array(self.data).shape)
+        # print(self.data[0][0])
+
+        self.train_data = self.data[:n_train_classses]
+        self.test_data = self.data[-n_test_classes:]
+
+    def fetch_batch(self, n_classes, batch_size, seq_length, type='train', sample_strategy='random', augment=True, label_type='one_hot'):
+
+        if type == 'train':
+            data = self.train_data
+        elif type == 'test':
+            data = self.test_data
+
+        # print('train', np.array(data).shape)
+        # print('data size', len(data))
+
+        # print(len(self.data[1]))
+        classes = [np.random.choice(range(len(data)), replace=False, size=n_classes) for _ in range(batch_size)]
+
+        # print('class size', len(classes))
+        # print('classes', np.array(classes).shape)
+        # print(classes)
+        if sample_strategy == 'random':         # #(sample) per class may not be equal (sec 7)
+            seq = np.random.randint(0, n_classes, [batch_size, seq_length])
+        elif sample_strategy == 'uniform':      # #(sample) per class are equal
+            seq = np.array([np.concatenate([[j] * int(seq_length / n_classes) for j in range(n_classes)])
+                   for i in range(batch_size)])
+            for i in range(batch_size):
+                np.random.shuffle(seq[i, :])
+        # print('seq_shape', seq.shape)
+        # print(seq)
+        # print('seq', seq[0])
+
+        self.rand_rotate_init(n_classes, batch_size)
+        # print(len(self.rand_rotate_map))
+
+        # print('data', len(data[0]))
+        seq_pic = [[self.augment(data[classes[i][j]][np.random.randint(0, len(data[classes[i][j]]))],
+                                 batch=i, c=j,
+                                 only_resize=not augment)
+                                 # batch=i, c=j)
+                   for j in seq[i, :]]
+                   for i in range(batch_size)]
+
+        # nseq_pic = np.array(seq_pic)
+        #
+        # print(nseq_pic.shape)
+
+        if label_type == 'one_hot':
+            seq_encoded = one_hot_encode(seq, n_classes)
+            seq_encoded_shifted = np.concatenate(
+                [np.zeros(shape=[batch_size, 1, n_classes]), seq_encoded[:, :-1, :]], axis=1
+            )
+
+        # print(np.array(seq_encoded).shape)
+        # print(seq_encoded_shifted.shape)
+
+        return seq_pic, seq_encoded_shifted, seq_encoded
+
+    def rand_rotate_init(self, n_classes, batch_size):
+        self.rand_rotate_map = np.random.randint(0, 4, [batch_size, n_classes])
+
+    def augment(self, image, batch, c, only_resize=False):
+        if only_resize:
+            image = ImageOps.invert(image.convert('L')).resize(self.image_size)
+
+        else:
+            rand_rotate = self.rand_rotate_map[batch, c] * 90                       # rotate by 0, pi/2, pi, 3pi/2
+            image = ImageOps.invert(image.convert('L')) \
+                .rotate(rand_rotate + np.random.rand() * 22.5 - 11.25,
+                        translate=np.random.randint(-10, 11, size=2).tolist()) \
+                .resize(self.image_size)   # rotate between -pi/16 to pi/16, translate bewteen -10 and 10
+        np_image = np.reshape(np.array(image, dtype=np.float32),
+                          newshape=(self.image_size[0] * self.image_size[1]))
+        max_value = np.max(np_image)    # normalization is important
+        if max_value > 0.:
+            np_image = np_image / max_value
+        # print(np_image)
+        # np_image = np_image / np.linalg.norm(np_image)
+
+        # numerator = np_image - np.min(np_image, 0)
+        # denominator = np.max(np_image, 0) - np.min(np_image, 0)
+        # np_image = numerator / (denominator + 1e-7)
+
+        # print(batch, c, len(np_image))
+
+        # print('2np', np_image)
+        # print('check', np_image.shape)
+        # print(np_image)
+        # print(np_image[5000])
+        return np_image
+
+# path = 'E:/Project/PycharmProjects/dogs/test_code/data/'
+#
+# data_loader = OmniglotDataLoader(
+#             data_dir=path,
+#             image_size=(20, 20),
+#             n_train_classses=13,
+#             n_test_classes=2
+#         )
+#
+# x_inst, x_label, y = data_loader.fetch_batch(5, 16, 50, type='train')
---
+++ util/binary_to_image.py
@@ -0,0 +1,264 @@
+import os
+import csv
+import shutil
+import array
+import numpy as np
+from scipy import misc
+
+# label_file = 'E:/Works/temp/model/label_list_re.csv'
+# file_folder = 'E:/Works/temp/model/mann_dataset_folder'
+#
+# f = open(label_file, 'r', encoding='utf-8')
+# rdr = csv.reader(f)
+#
+# category_set = set()
+# category_dict = dict()
+#
+# for line in rdr:
+#     category = line[1]
+#     category_set.add(category)
+#
+# for category in category_set:
+#     path = os.path.join(file_folder, category)
+#     if not os.path.exists(path):
+#         os.makedirs(path)
+#
+# f2 = open(label_file, 'r', encoding='utf-8')
+# rdr2 = csv.reader(f2)
+#
+# for line in rdr2:
+#     md5 = line[0]
+#     category_folder = line[1]
+#     src = os.path.join(file_folder, md5)
+#     dest = os.path.join(file_folder, category_folder)
+#     shutil.move(src, dest)
+#     print('ok')
+
+# from gensim.models import doc2vec
+# import numpy as np
+# from PIL import Image
+# import numpy as np
+# import zipfile
+# from scipy import misc
+#
+# path = 'E:/Project/PycharmProjects/dogs/model/bin_model/basicblock_by_file_bin2vec.model'
+# label_file = 'E:/Works/temp/model/label_list_re.csv'
+#
+# f = open(label_file, 'r', encoding='utf-8')
+# rdr = csv.reader(f)
+#
+# hash_list = list()
+# model = doc2vec.Doc2Vec.load(path)
+
+'''
+doc2vec -> Image
+'''
+#
+# count = 1
+# for line in rdr:
+#     hash = line[0]
+#     x = np.array(model.docvecs[hash])
+#     w, h = 20, 20
+#     data = np.zeros((h, w), dtype=np.uint8)
+#     data = np.reshape(x, (-1, 20))
+#     img = Image.fromarray(data, 'L')
+#     filename = hash+'.png'
+#     img.save('E:/Works/png/'+filename)
+#     print('complete', count,'/3200')
+#     count = count + 1
+
+
+'''
+Binary -> Image
+'''
+
+# data_folder = 'E:/Works/Data2/tt/'
+# save_folder = 'E:/Works/Data2/tt2/'
+# # mann_dataset_filename = 'mann_dataset.zip'
+#
+# count = 1
+# for (path, dir, files) in os.walk(data_folder):
+#     for filename in files:
+#         path = data_folder+filename
+#         f = open(path, 'rb')
+#         ln = os.path.getsize(path)
+#
+#         width = int(ln**0.5)
+#         # width = 256
+#
+#         rem = ln % width
+#
+#         a = array.array("B")
+#
+#         a.fromfile(f, ln-rem)
+#         # print(len(a)/width)
+#         f.close()
+#
+#         g = np.reshape(a, (int(len(a)/width), width))
+#         g = np.uint8(g)
+#         sp = save_folder+filename+'.png'
+#         # print(path)
+#         misc.imsave(sp, g)
+#         print('complete', count, '/1676')
+#         count = count + 1
+
+
+'''
+temp code
+'''
+# label_file = 'E:/Works/Data2/label.csv'
+#
+# # all_path = 'E:/Works/Data/backup/all/all'
+# sample_path = 'E:/Works/Data2/sample_png_gray'
+# sample_folder_path = 'E:/Works/Data2/sample_png_gray_folder'
+#
+# f = open(label_file, 'r', encoding='utf-8')
+# rdr = csv.reader(f)
+#
+# file_dict = dict()
+# for (path, dir, files) in os.walk(sample_path):
+#     for file in files:
+#         s_ex_ext = os.path.splitext(file)
+#         p = os.path.join(path, file)
+#         file_dict[s_ex_ext[0]] = p
+#         # print(p)
+#
+# # print(file_dict)
+#
+#
+# for line in rdr:
+#     hash = line[0]
+#     category = line[1]
+#     directory = os.path.join(sample_folder_path, category)
+#
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#
+#     dest_path = os.path.join(sample_folder_path, category, hash+'.png')
+#     if file_dict.get(hash) != None:
+#         src_path = file_dict[hash]
+#         shutil.move(src_path, dest_path)
+#
+
+# print(len(file_list))
+
+
+
+# for line in rdr:
+#     hash = line[0]
+#     category = line[1]
+#     directory = os.path.join(png_dataset, category)
+#
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#
+#     src = os.path.join(png_dataset, hash+'.png')
+#     dest = os.path.join(png_dataset, directory, hash)
+#     shutil.move(src, dest)
+
+'''
+change gray -> binary image
+'''
+
+# import cv2
+#
+# data_folder = 'E:/Works/Data2/tt/'
+# save_folder = 'E:/Works/Data2/tt2/'
+#
+# count = 1
+# for (path, dir, files) in os.walk(data_folder):
+#     for filename in files:
+#         path = data_folder+filename
+#         im_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+#         (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#         thresh = 127
+#         im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+#         sp = save_folder + filename
+#         cv2.imwrite(sp, im_bw)
+#         print('complete', count, '/1676')
+#         count = count + 1
+
+
+'''
+change Binary -> binary image file
+'''
+import cv2
+
+# data_folder = 'E:/Works/Data2/sample'
+# save_folder = 'E:/Works/Data2/sample_png'
+#
+# count = 1
+# for (path, dir, files) in os.walk(data_folder):
+#     for filename in files:
+#         category = path.split('\\')[1]
+#         p = os.path.join(path, filename)
+#         f = open(p, 'rb')
+#         ln = os.path.getsize(p)
+#
+#         width = int(ln**0.5)
+#         # width = 256
+#
+#         rem = ln % width
+#
+#         a = array.array("B")
+#
+#         a.fromfile(f, ln-rem)
+#         # print(len(a)/width)
+#         f.close()
+#
+#         g = np.reshape(a, (int(len(a)/width), width))
+#         g = np.uint8(g)
+#
+#         sp = os.path.join(save_folder, category)
+#         sp2 = os.path.join(save_folder, category, filename+'.png')
+#         if not os.path.exists(sp):
+#             os.makedirs(sp)
+#
+#         # sp = save_folder+filename+'.png'
+#         # print(path)
+#         misc.imsave(sp2, g)
+#         print('complete', count, '/', '1240')
+#         count = count + 1
+
+
+
+# data_folder = 'E:/Works/Data2/sample_png'
+# save_folder = 'E:/Works/Data2/sample_binary_100'
+#
+# count = 1
+# for (path, dir, files) in os.walk(data_folder):
+#     for filename in files:
+#         category = path.split('\\')[1]
+#         p = os.path.join(path, filename)
+#
+#         im_gray = cv2.imread(p, cv2.IMREAD_GRAYSCALE)
+#         (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+#         thresh = 100
+#         im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+#
+#         sp = os.path.join(save_folder, category)
+#         if not os.path.exists(sp):
+#             os.makedirs(sp)
+#         sp2 = os.path.join(save_folder, category, filename)
+#         # sp_t = os.path.join(save_folder, filename + '.png')
+#         cv2.imwrite(sp2, im_bw)
+#         print('complete', count, '/1240')
+#         count = count + 1
+
+
+# data_folder = 'E:/Works/Data2/sample_binary'
+# save_folder = 'E:/Works/Data2/sample_binary_resize'
+#
+# count = 1
+# for (path, dir, files) in os.walk(data_folder):
+#     for filename in files:
+#         category = path.split('\\')[1]
+#         p = os.path.join(path, filename)
+#
+#         img = cv2.imread(p)
+#         shrink = cv2.resize(img, dsize=(20, 20), interpolation=cv2.INTER_AREA)
+#
+#         sp_t = os.path.join(save_folder, filename + '.png')
+#         cv2.imwrite(sp_t, shrink)
+#         print('complete', count, '/420')
+#         count = count + 1
